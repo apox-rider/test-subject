d2cd7173321ce293d744085dd060dfcf950e36fd
apox-rider <apolinaryavith@gmail.com>
2026-01-21 18:33:19 +0300

    Here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ import { supabase } from "../components/supabase-client";
             <div className="mb-6">
                 <input 
                 type="password" 
-                id="confirm_password" 
+                id="confirmPassword" 
                 name="confirmed_password"
                 onChange={(e)=>setRegister((prev)=>({...prev, confirmed_password:e.target.value}))}
                 className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" 
