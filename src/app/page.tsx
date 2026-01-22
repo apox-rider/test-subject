109bce71ec4816b77ee10de41886e28f37ba7f79
apox-rider <apolinaryavith@gmail.com>
2026-01-22 14:16:10 +0300

    current

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -162,7 +162,7 @@ import { supabase } from "../components/supabase-client";
               }}>
                 <div>
                   <h3>Id: {data.id}</h3>
-                  <h3>{data.first_name}</h3>
+                  <h3>First name: {data.first_name}</h3>
                   <h3>{data.last_name}</h3>
                   <h3>{data.email}</h3>
                   <h3>{data.phone}</h3>
