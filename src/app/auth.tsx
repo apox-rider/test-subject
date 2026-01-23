d5e8af3e130a964ce1d67d190c3277392677f315
apox-rider <apolinaryavith@gmail.com>
2026-01-23 10:48:06 +0300

    flow

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -47,7 +47,7 @@ import { error } from "console";
                     onChange={(e: ChangeEvent<HTMLInputElement>)=>
                         setEmail(e.target.value)}
                     className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" 
-                    placeholder="Email..." 
+                    placeholder="Email" 
                     required />
                 </div>
                 <div>
