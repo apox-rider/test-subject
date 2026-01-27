7cf251016a72026fc5eeeae09f190cc59921af64
apox-rider <apolinaryavith@gmail.com>
2026-01-27 19:42:33 +0300

    mmwomafdsa

--- src/app/sample.tsx
+++ src/app/sample.tsx
@@ -95,7 +95,7 @@ import { supabase } from "../components/supabase-client";
                     name="company" 
                     onChange={(e)=>setRegister((prev)=>({...prev, company:e.target.value}))}
                     className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" 
-                    placeholder="Company" 
+                    placeholder="Company Registered" 
                     required />
                 </div>  
                 <div>
